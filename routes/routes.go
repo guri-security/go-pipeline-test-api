15507b04dd58fe2bb644fa776a857477a655f3c7
felipemgurian <felipemgurian@hotmail.com>
2022-09-30 20:37:08 -0300

    add api

--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,11 @@ func getRoot(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "get\n")
 }
 
+func getApi(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "getApi\n")
+}
+
 func Load() {
 	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/api", getApi)
 }
