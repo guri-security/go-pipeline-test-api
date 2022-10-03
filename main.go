16aa0f6a867ce8d205167a38e7ca067ef243c253
felipemgurian <felipemgurian@hotmail.com>
2022-10-03 20:29:58 -0300

    fix

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/guri-security/go-pipline-test-api/routes"
 )
 
 func main() {
 	routes.Load()
-	http.ListenAndServe(":8888", nil)
+
+	server := &http.Server{
+		Addr:              ":8888",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
 }
